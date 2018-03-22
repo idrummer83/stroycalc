db09b0463cc04bf9ec5d89b494adf20a4013badc
idrummer83 <vodaadminwater@gmail.com>
2018-03-22 22:07:21 +0200

    work

--- start.py
+++ start.py
@@ -1,27 +1,17 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, g
 from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 # from sqlalchemy.orm import relationship
 
 app = Flask('__name__')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://calc:calc@localhost/calc'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/database.db'
 db = SQLAlchemy(app)
 
+from collections import Counter
+from itertools import chain
 
-class Category(db.Model):
-    __tablename__ = 'category'
-    id = db.Column('id', db.Integer, primary_key=True)
-    name_category = db.Column('name_cat', db.String(50), unique=True)
-    # items = db.relationship('Item', backref='category', lazy=True,)
-
-
-class Item(db.Model):
-    __tablename__= 'item'
-    id = db.Column('id', db.Integer, primary_key=True)
-    name_item = db.Column('name_item', db.String(200), unique=False)
-    price_item = db.Column('price_item', db.Integer, unique=False)
-    cat_id = db.Column('cat_id', db.Integer)
-
-
+from models.models import *
 
 @app.route('/')
 def welcome():
@@ -34,8 +24,8 @@ def create_category():
     if request.method == 'POST':
         cat = request.form.get('cat', '')
         if cat:
-            c = Category(name_category=cat)
-            db.session.add(c)
+            category = Category(name_category=cat)
+            db.session.add(category)
             db.session.commit()
         return redirect('/category')
     return render_template('category.html',all_cat = Category.query.all())
@@ -49,8 +39,8 @@ def create_item():
         item = request.form.get('item', '')
         price = request.form.get('price', '')
         if cat and item and price:
-            c_item = Item(name_item=item,price_item=price,cat_id=cat)
-            db.session.add(c_item)
+            category_item = Item(name_item=item,price_item=price,cat_id=cat)
+            db.session.add(category_item)
             db.session.commit()
         return redirect('/item')
     return render_template('item.html', all_cat = Category.query.all(), all_item = Item.query.all())
@@ -60,36 +50,38 @@ def create_item():
 def calc_page():
     all_cat = Category.query.all()
     all_item = Item.query.all()
-    l=[]
+    list_all_checked=[]
     if request.method == 'POST':
-        all = request.form.getlist('price')
+        all_checked = request.form.getlist('price')
 
-        for a in all:
-            l.append(a.split('_'))
+        # creating all checked inputs's
+        for a in all_checked:
+            list_all_checked.append(a.split('_'))
 
-        from collections import Counter
-        from itertools import chain
-        z = []
-        counts = Counter(chain.from_iterable(l))
+        # creating list of id's and price
+        list_id_counter = []
+        counts = Counter(chain.from_iterable(list_all_checked))
         for s, x in counts.items():
             if 'id' in s:
-                z.append([s, int(x)])
-        w = []
-        for i in z:
+                list_id_counter.append([s, int(x)])
+
+        # calculating dict with id and calculated price
+        list_calculate = []
+        for i in list_id_counter:
             k = 0
-            for a in l[:i[1]:]:
+            for a in list_all_checked[:i[1]:]:
                 k += int(a[0])
                 u = a[1]
-            del l[:i[1]:]
-            w.append([u, k])
-        dct = [dict((k[0], k[1]) for k in w)]
+            del list_all_checked[:i[1]:]
+            list_calculate.append([u, k])
+        dict_list_calculate = [dict((k[0], k[1]) for k in list_calculate)]
 
-        fp = []
+        # calculating full price
         f=0
-        for fp in w:
+        for fp in list_calculate:
             f += fp[1]
 
-        return render_template('calc.html', all_cat = all_cat, all_item = all_item, dct=dct, full = f)
+        return render_template('calc.html', all_cat = all_cat, all_item = all_item, dct=dict_list_calculate, full = f)
 
     return render_template('calc.html', all_cat = all_cat, all_item = all_item)
 
