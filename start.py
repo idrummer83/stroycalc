cb7ef6d9a3773f4023a4971654f7706096b09c4e
idrummer83 <vodaadminwater@gmail.com>
2018-03-23 21:49:08 +0200

    fix

--- start.py
+++ start.py
@@ -9,15 +9,6 @@ db = SQLAlchemy(app)
 
 from collections import Counter
 from itertools import chain
-from sqlalchemy.engine import Engine
-from sqlalchemy import event
-
-@event.listens_for(Engine, "connect")
-def set_sqlite_pragma(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("PRAGMA foreign_keys=ON")
-    cursor.close()
-
 from models.models import *
 
 @app.route('/')
